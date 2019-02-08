ab6c73a517591823f1715e9c35a2a91ab0d4a30e
Андрей <andrey.mardash@gmail.com>
2019-02-08 18:42:06 +0300

    Updates

---
+++ precache-manifest.125e79ea758382fe38fca9159ee64cd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ce38f75bfc080b63ec0",
+    "url": "/bananas-and-shit/static/css/main.3984fedd.chunk.css"
+  },
+  {
+    "revision": "6ce38f75bfc080b63ec0",
+    "url": "/bananas-and-shit/static/js/main.6ce38f75.chunk.js"
+  },
+  {
+    "revision": "e104236579b56550ba7e",
+    "url": "/bananas-and-shit/static/js/1.e1042365.chunk.js"
+  },
+  {
+    "revision": "3787fcc6f48baf0cb27a",
+    "url": "/bananas-and-shit/static/js/runtime~main.3787fcc6.js"
+  },
+  {
+    "revision": "fbf2d4db30f44b0e24952b7f3dc36b37",
+    "url": "/bananas-and-shit/index.html"
+  }
+];
