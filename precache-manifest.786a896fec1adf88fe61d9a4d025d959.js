17c4ddb883ace77747f713783156550a930999b0
Андрей <andrey.mardash@gmail.com>
2019-02-11 20:12:53 +0300

    Updates

---
+++ precache-manifest.786a896fec1adf88fe61d9a4d025d959.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2079be8e12f263f189d3",
+    "url": "/bananas-and-shit/static/css/main.c662fd7d.chunk.css"
+  },
+  {
+    "revision": "2079be8e12f263f189d3",
+    "url": "/bananas-and-shit/static/js/main.2079be8e.chunk.js"
+  },
+  {
+    "revision": "b4e6a5970f319a6c60f5",
+    "url": "/bananas-and-shit/static/js/1.b4e6a597.chunk.js"
+  },
+  {
+    "revision": "3787fcc6f48baf0cb27a",
+    "url": "/bananas-and-shit/static/js/runtime~main.3787fcc6.js"
+  },
+  {
+    "revision": "09a2ca25d10a4f4d1a15cf1f74e6a3dc",
+    "url": "/bananas-and-shit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bananas-and-shit/precache-manifest.4eca9e98ab4482368b010c6837fa64c4.js"
+  "/bananas-and-shit/precache-manifest.786a896fec1adf88fe61d9a4d025d959.js"
 );
 
 workbox.clientsClaim();
