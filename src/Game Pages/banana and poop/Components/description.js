41e6d018b8fb0ec0720d0b8e4a14bc015dfc9c9d
andrey_mardash <andrey.mardash@gmail.com>
2020-09-18 23:49:57 +0300

    Created a new file:   Description_Article.js Change in description.js:   + <Description_Article />

--- src/Game Pages/banana and poop/Components/description.js
+++ src/Game Pages/banana and poop/Components/description.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import '../../../index.css';
-import {inputChange} from '../../../Functions/App_js_Functions/Banana_and_Poop_functions';
+// Components
 import TopNav from "./TopNav";
+import Description_Article from "./Description_Article";
+// Functions
+import {inputChange} from '../../../Functions/App_js_Functions/Banana_and_Poop_functions';
+
 
 class description extends Component {
   constructor(props) {
@@ -26,32 +30,7 @@ class description extends Component {
     return (
       <div>
         <TopNav money={this.props.money} back={() => this.props.menu()} addMoney={() => this.props.addMoney()}/>
-        <article>
-          <h1>{JSON.parse(localStorage.getItem('user')) !== 'stranger' ? `Привет, ${JSON.parse(localStorage.getItem('user')).name}` : 'Description'}</h1>
-          <h3>Hello my dear friend. Today I'm going to show you my first computer game and explain the rules of it.</h3>
-          <p>
-            1. There are 4 levels.
-            <br/>
-            2. You choose one of the five cherries.
-            <br/>
-            3. After that you will be shown what exactly you have chosen. Was it shit or a banana?
-            <br/>
-            4. Then you will be offered a choice. You can take money and finish the game or continue it for a greater
-            gain.
-            <br/>
-            5. If you've got crap you can't go on playing.
-            <br/>
-            6. The greater level you have, the more crap is hidden beside the cherries.
-            <br/>
-            7. Your bet can't be lower 10$.
-            <br/>
-            8. All the money you won are stored.
-            <br/>
-            9. If you lost all your money, you won't be able to play this game anymore.
-            <br/>
-            Good luck!!!
-          </p>
-        </article>
+        <Description_Article/>
         <br/>
         <div className="input-group mb-3 ssize">
           <input type="text" className="form-control input" placeholder="Your bet"
